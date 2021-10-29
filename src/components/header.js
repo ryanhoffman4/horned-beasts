bd546e78cf3a6058a4ea3bcc7fefb46e311b59f5
ryanhoffman4 <ryanhoffman4@gmail.com>
2021-10-29 00:43:21 -0700

    finished lab-04

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import DropMenu from './DropMenu.js';
 
 export default class Header extends React.Component {
+
   render() {
     return(
       <div>
         <h1>Horned Beasts</h1> 
+        <DropMenu filterArray={this.props.filterArray}/>
       </div>
     )
   }
